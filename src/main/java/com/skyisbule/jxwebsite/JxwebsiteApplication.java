1d5ca8200b9bbe530696dd253c3f9b1593e5f362
skyisbule <heqiut@qq.com>
2018-03-17 23:53:37 +0800

    init

---
+++ src/main/java/com/skyisbule/jxwebsite/JxwebsiteApplication.java
@@ -0,0 +1,12 @@
+package com.skyisbule.jxwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JxwebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JxwebsiteApplication.class, args);
+	}
+}

07812ceacb416915f59d6be72795b2dec27ded34
skyisbule <heqiut@qq.com>
2018-03-19 18:22:42 +0800

    后台模板

--- src/main/java/com/skyisbule/jxwebsite/controller/view.java
+++ src/main/java/com/skyisbule/jxwebsite/controller/view.java
@@ -3,6 +3,7 @@ package com.skyisbule.jxwebsite.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by skyisbule on 2018/3/18.
@@ -14,6 +15,21 @@ public class view {
     @RequestMapping(value = "/login",method = RequestMethod.GET)
     public String login(){
         return "login";
+    }
+
+    @RequestMapping("/admin")
+    public String adminIndex(){
+        return "admin_index";
+    }
+
+    @RequestMapping("/admin/main")
+    public String adminMain(){
+        return "admin_main";
+    }
+
+    @RequestMapping("/admin/editor")
+    public String adminEditor(){
+        return "editor";
     }
 
 }
